c97513118ced140ebb668b8e3762b3bdd11a5fad
Kevin Denamganaï <denamganai.kevin@gmail.com>
2015-11-12 19:06:53 +0100

    dernieres modifications de ICommand.h etc...

--- src/ICommand.h
+++ src/ICommand.h
@@ -78,6 +78,143 @@ class CameraCommand : public ICommand
 	
 };
 
+class LoadMapCommand : public ICommand
+{
+	private :
+	std::string fileName;
+	
+	public :
+	LoadMapCommand(std::string fileName_) : ICommand(TCLoadMap), fileName(fileName_)
+	{
+	
+	}
+	
+	~LoadMapCommand()
+	{
+	
+	}
+	
+	//------------------------------------
+	const std::string getFileName() const
+	{
+		return fileName;
+	}
+};
 
+
+class NewCommand : public ICommand
+{
+	public :
+	NewCommand() : ICommand(TCNew)
+	{
+	
+	}
+
+	~NewCommand()
+	{
+	
+	}
+};
+
+
+class LoadCommand : public ICommand
+{
+	private :
+	std::string file_name;
+	
+	public :
+	LoadCommand(std::string file_name_) : ICommand(TCLoad), file_name(file_name_)
+	{
+	
+	}
+	
+	~LoadCommand()
+	{
+	
+	}
+	
+	//------------------------------------
+	const std::string getFileName() const
+	{
+		return file_name;
+	}
+
+};
+
+
+class PauseCommand : public ICommand
+{
+	public :
+	PauseCommand() : ICommand(TCPause)
+	{
+	
+	}
+
+	~PauseCommand()
+	{
+	
+	}
+
+};
+
+class GoOnCommand : public ICommand
+{
+	public :
+	GoOnCommand() : ICommand(TCGoOn)
+	{
+	
+	}
+
+	~GoOnCommand()
+	{
+	
+	}
+
+};
+
+
+class QuitCommand : public ICommand
+{
+	public :
+	QuitCommand() : ICommand(TCQuit)
+	{
+	
+	}
+
+	~QuitCommand()
+	{
+	
+	}
+
+};
+
+class RigidBodysCommand : public ICommand
+{
+	private :
+	enum IDJoint joint;
+	enum StateJoint jointState;
+		
+	public :
+	RigidBodyCommand()
+	{
+	
+	}
+	
+	~RigidBodyCammand()
+	{
+	
+	}	
+	
+	//------------------------------
+	const enum IDJoint getIDJoint() const
+	{
+		return joint;
+	}
+	
+	const enum StateJoint getStateJoint() const
+	{
+		return jointState;
+	}
+};
 
 #endif
---
+++ src/IDJoint.h
@@ -0,0 +1,26 @@
+#ifndef IDJOINT_H
+#define IDJOINT_H
+
+enum IDJoint{
+	IJMainDroite,
+	IJMainGauche,
+	IJPoigneDroit,
+	IJPoigneGauche,
+	IJCoudeDroit,
+	IJEpauleGauche,
+	IJEpauleDroit,
+	IJEpauleGauche,
+	IJNuque,
+	IJBusteRotation,
+	IJAbdominauxLateraDroit,
+	IJAbdominauxLateraGauche,
+	IJAbdominauxCentral,
+	IJHancheDroite,
+	IJHancheGauche,
+	IJGenouDroit,
+	IJGenouGAuche,
+	IJChevilleDroite,
+	IJChevilleGauche
+}
+
+#endif
